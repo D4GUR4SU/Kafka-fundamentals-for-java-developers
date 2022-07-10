f424806ffef3de515216d7e843f4109812261960
Dagurasu56 <douglas_search@hotmail.com>
2022-07-10 12:03:54 -0300

    Create producers and consumers

---
+++ orderconsumer/src/main/java/com/dagurasu/kafka/orderconsumer/App.java
@@ -0,0 +1,13 @@
+package com.dagurasu.kafka.orderconsumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
