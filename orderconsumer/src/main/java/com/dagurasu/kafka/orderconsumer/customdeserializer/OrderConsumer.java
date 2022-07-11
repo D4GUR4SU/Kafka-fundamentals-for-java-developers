9d884e77b10e97e18ee1d111f18127003528a5fe
Dagurasu56 <douglas_search@hotmail.com>
2022-07-10 21:15:16 -0300

    Consumer groups rebalancing, Offsets commits...

--- orderconsumer/src/main/java/com/dagurasu/kafka/orderconsumer/customdeserializer/OrderConsumer.java
+++ orderconsumer/src/main/java/com/dagurasu/kafka/orderconsumer/customdeserializer/OrderConsumer.java
@@ -1,13 +1,20 @@
 package com.dagurasu.kafka.orderconsumer.customdeserializer;
 
 import java.time.Duration;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.clients.consumer.OffsetCommitCallback;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 public class OrderConsumer {
@@ -20,13 +27,32 @@ public class OrderConsumer {
 		props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, OrderDeserializer.class.getName());
 		props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "OrderGroup");
+		// props.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "2000");
+		props.setProperty("auto.commit.offset", "false");
 
+		Map<TopicPartition, OffsetAndMetadata> currentOffsets = new HashMap<>();
 		KafkaConsumer<String, Order> consumer = new KafkaConsumer<>(props);
-		consumer.subscribe(Collections.singletonList(TOPIC_NAME));
+
+		class RebalanceHandler implements ConsumerRebalanceListener {
+
+			@Override
+			public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+				consumer.commitSync(currentOffsets);
+			}
+
+			@Override
+			public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+
+			}
+		}
+
+		consumer.subscribe(Collections.singletonList(TOPIC_NAME), new RebalanceHandler());
 
 		try {
 			while (true) {
 				ConsumerRecords<String, Order> records = consumer.poll(Duration.ofSeconds(20));
+				int count = 0;
+
 				for (ConsumerRecord<String, Order> record : records) {
 					String customerName = record.key();
 					Order order = record.value();
@@ -34,6 +60,23 @@ public class OrderConsumer {
 					System.out.println("Product: " + order.getProduct());
 					System.out.println("Quantity: " + order.getQuantity());
 					System.out.println("Partition: " + record.partition());
+
+					currentOffsets.put(new TopicPartition(record.topic(), record.partition()),
+							new OffsetAndMetadata(record.offset() + 1));
+
+					if (count % 10 == 0) {
+						consumer.commitAsync(currentOffsets, new OffsetCommitCallback() {
+
+							@Override
+							public void onComplete(Map<TopicPartition, OffsetAndMetadata> offsets,
+									Exception exception) {
+								if (exception != null) {
+									System.out.println("Commit failed for offset" + offsets);
+								}
+							}
+						});
+					}
+					count++;
 				}
 			}
 		} finally {
